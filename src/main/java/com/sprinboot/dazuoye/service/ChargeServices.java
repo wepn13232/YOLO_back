075543583d7a0bf2bbea37a86fc35dc1fa00b1cd
linqiyuan <466214222@qq.com>
2020-01-18 23:10:37 +0800

    初始化

--- src/main/java/com/sprinboot/dazuoye/service/ChargeServices.java
+++
@@ -1,9 +0,0 @@
-package com.sprinboot.dazuoye.service;
-
-import org.springframework.stereotype.Service;
-
-public interface ChargeServices {
-
-//    查询余额
-    int checkCashLeft(String username) throws Exception;
-}
